8aa40ac3b7ff4976b15454110e38e40523838950
Ninad Dhulap <ninadkdhulap17@outlook.com>
2025-11-06 15:20:05 +0530

    Update Decimal to binary logic and it now supports signed values

--- [01] Basic/02_Decimal_To_Binary.cpp
+++ [01] Basic/02_Decimal_To_Binary.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 #include <math.h>
+#include <string>
+#include <bitset>
 using namespace std;
+
+// Using bitset - for getting correct binary of all signed/unsigned int
+bitset<32> usingBitset(int decNum) {
+    return bitset<32>(decNum); 
+}
+
+string manualBitsetLike(int decNum) {
+    string bits = "";
+    
+    for(int i = 31; i >=0; i--) {
+        int bit = (decNum >> i) & 1;
+        bits += (bit ? "1": "0");
+    }
+
+    return bits;
+}
 
 // Time complexity for both functions is O(log(n))
 // Space complexit is O(1)
@@ -41,7 +59,12 @@ int main() {
     cout << "Enter a number: ";
     cin >> decNum;
 
-    cout << "Binary of " << decNum << " is " << decToBin(decNum) << endl;
-    cout << "Binary of " << decNum << " is " << usingDivBy2Method(decNum) << endl;
+    if(decNum >= 0) {
+        // if number is not -ve then only run following functions
+        cout << "Binary of " << decNum << " is " << decToBin(decNum) << endl;
+        cout << "Binary of " << decNum << " is " << usingDivBy2Method(decNum) << endl;
+    }
+    cout << "Binary of " << decNum << " using bitset is " << usingBitset(decNum) << endl;
+    cout << "Binary of " << decNum << " using bitset manual implementation is " << manualBitsetLike(decNum) << endl;
     return 0;
 }
